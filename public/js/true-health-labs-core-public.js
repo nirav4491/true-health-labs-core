d62538670a61f27e145b361c76e22e584ecda244
Nivs <nirmehta4491@gmail.com>
2023-09-06 16:14:09 +0530

    JS Changes

--- public/js/true-health-labs-core-public.js
+++ public/js/true-health-labs-core-public.js
@@ -4,8 +4,8 @@ jQuery( document ).ready( function( $ ) {
 		$( document ).on( 'focusout', '.checkout #billing_email', function() {
 			var email = $( this ).val();
 			if ( ! validEmail( email ) ) {
-				var html = '<span class="billing_email_error" style="color:red;">Please Enter Valid Email.</span>';
-				$( html ).insertAfter( '.checkout #billing_email' );
+				var html = '<span class="billing_email_error" style="color:red;padding:10px;">Please Enter Valid Email.</span>';
+				$( html ).insertAfter( '.checkout #billing_email_field span' );
 				setTimeout( function() {
 					$( '.billing_email_error' ).remove();
 				}, 3000 );
@@ -15,22 +15,28 @@ jQuery( document ).ready( function( $ ) {
 				type: 'POST',
 				data: { 'email': email, 'action': 'cs_check_email_exists_ajax' },
 				dataType: 'json',
+				beforeSend: function() {
+					$( '.custom_loader' ).show();
+				},
 				success: function( response ) {
 					if ( 'email-exists' === response.data.code ) {
-						var html = '<span class="billing_email_error">' + response.data.message + '</span>';
+						var html = '<span class="billing_email_error" style="padding:10px;">' + response.data.message + '</span>';
 						if ( $( '#billing_email_field' ).find( '.billing_email_error' ).length !== 0 ) {
 							$( '.billing_email_error' ).html( response.data.message );
 						} else {
-							$( html ).insertAfter( '.checkout #billing_email' );
+							$( html ).insertAfter( '.checkout #billing_email_field span' );
 						}
-						$( '.checkout .input-text:not(:first)' ).prop("disabled", true);
+						$( '.checkout #customer_details .form-row:not(:first)' ).hide();
 						$( '.checkout .button' ).prop("disabled", true);
+						
 					} else {
 						$( '.billing_email_error' ).remove();
-						$( '.checkout .input-text:not(:first)' ).prop("disabled", false);
+						$( '.checkout #customer_details .form-row:not(:first)' ).show();
 						$( '.checkout :button' ).prop("disabled", false);
 					}
-
+				},
+				complete: function() {
+					$( '.custom_loader' ).hide();
 				}
 			} );
 
